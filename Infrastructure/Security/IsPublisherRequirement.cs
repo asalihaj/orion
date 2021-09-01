96527bca0ea221bf0d89461011a4c69875f22546
asalihaj <as48243@ubt-uni.net>
2021-09-01 22:18:12 +0200

    Added offer publisher policy

--- Infrastructure/Security/IsPublisherRequirement.cs
+++ Infrastructure/Security/IsPublisherRequirement.cs
@@ -35,12 +35,12 @@ namespace Infrastructure.Security
                 var user = _context.Users.SingleOrDefault(x => x.UserName == currentUserName);
 
                 if (user == null)
-                    throw new RestException(HttpStatusCode.Unauthorized, new {auth = "Unauthorized request 1"});
+                    throw new RestException(HttpStatusCode.Unauthorized, new {auth = "Please log in to edit or delete the offer"});
 
                 var company = _context.Companies.FindAsync(user.Id).Result;
 
                 if (company == null)
-                    throw new RestException(HttpStatusCode.Unauthorized, new {auth = "Unauthorized request 2"});
+                    throw new RestException(HttpStatusCode.Forbidden, new {auth = "You don't have premission to edit offers"});
 
                 var offerId = Guid.Parse(authContext.RouteData.Values["id"].ToString());
 
@@ -50,6 +50,9 @@ namespace Infrastructure.Security
                     throw new RestException(HttpStatusCode.BadRequest, new {offer = "Could not find offer"});
 
                 var publisher = offer.CompanyId;
+
+                if (publisher != company.UserId)
+                    throw new RestException(HttpStatusCode.Forbidden, new {auth = "You are not the publisher of this offer"});
 
                 if (publisher == company.UserId)
                     context.Succeed(requirement);
